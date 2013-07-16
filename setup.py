d45b0141ffb4835af0b2d464fdd303ab63b6a2d0
Vikash Dat <vikashdat@new-host.home>
2013-07-15 22:44:43 -0400

    initial project setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+required_modules = [
+    "django == 1.5.1",
+    ]
+
+
+setup(
+    name="Citi Digits",
+    version="0.1",
+    description="",
+    author="Vikash Dat",
+    author_email="dat.vikash@gmail.com",
+    packages=find_packages(exclude=['test']),
+    install_requires=required_modules
+)
