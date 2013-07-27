049f7b75f2fb368d636058b50141229a670ded01
Vikash Dat <vikash.dat@controlgroup.com>
2013-07-27 16:12:46 -0400

    added in neighborhood layer

---
+++ mit_civic/citi_digits/static/js/CityDigitsMaps.js
@@ -0,0 +1,70 @@
+function CityDigitsMap() {
+    //init values
+    this.closeTooltip = null;
+    this.neighborhoodLayer = null;
+
+    //load map
+    var basemap = "sw2279.NYCLotto";
+    this.map = L.mapbox.map('map', basemap);
+
+    //set params
+    this.height = $(window).height()-$(".navbar").height();
+    this.width = $(window).width();
+
+     //disable unwanted events
+    this.map.touchZoom.disable();
+    this.map.doubleClickZoom.disable();
+    this.map.scrollWheelZoom.disable();
+    this.popup = new L.Popup({ autoPan: false });
+
+}
+
+//CityDigitsMap.prototype.onEachFeature = function(feature, layer) {
+//    console.log("ON EACH FEATURE");
+////       );
+//}
+
+CityDigitsMap.prototype.loadLayers =  function (){
+    var self = this;
+    console.log("LOADING LAYERS");
+    //show map ui nav
+    $("#map-nav").load("/map/nav/");
+
+    //add neighborhoods
+    this.neighborhoodLayer = L.geoJson(nyc_neighborhoods,{
+        style : {
+            weight: 0,
+            opacity: 0.1
+        }
+    }).addTo(this.map);
+    this.neighborhoodLayer.on('mouseover', function(e) {
+        self.mapMouseMove(e);
+    });
+    this.neighborhoodLayer.on('mouseout', function(e) {
+        self.mapMouseOut(e);
+    });
+}
+
+CityDigitsMap.prototype.resizeMap = function(){
+    $("#map").height(this.height);
+    $("#map").width(this.width);
+}
+
+CityDigitsMap.prototype.mapMouseMove = function (ev){
+    //get layer
+    var layer = ev.layer;
+    //get lat/long
+    this.popup.setLatLng(ev.latlng);
+    this.popup.setContent(layer.feature.properties.N_Name);
+    //display popup
+    if (!this.popup._map) this.popup.openOn(this.map);
+    window.clearTimeout(this.closeTooltip);
+}
+
+CityDigitsMap.prototype.mapMouseOut = function(e) {
+    var self = this;
+    this.neighborhoodLayer.resetStyle(e.target);
+    this.closeTooltip = window.setTimeout(function() {
+        self.map.closePopup();
+        }, 100);
+}
--- mit_civic/citi_digits/static/js/application.js
+++ mit_civic/citi_digits/static/js/application.js
@@ -9,47 +9,12 @@
  */
 
 $().ready(new function(){
-    init_map();
-    resize_map();
+    var myMap = new CityDigitsMap();
+    myMap.resizeMap();
+    myMap.loadLayers();
+
 });
 
-/*
-  Mapbox.js related functionality
- */
-
-function resize_map(){
-    $("#map").height( $(window).height()-$(".navbar").height());
-}
-
-function init_map(){
-    //load map
-    var basemap = "sw2279.NYCLotto";
-    var map = L.mapbox.map('map', basemap);
-
-    //disable unwanted events
-    map.touchZoom.disable();
-    map.doubleClickZoom.disable();
-    map.scrollWheelZoom.disable();
-
-    //show map ui nav
-    $("#map-nav").load("/map/nav/");
-//    var ui = document.getElementById('map-ui');
-//
-//    //add layers
-//        L.mapbox.tileLayer('').addTo(map);
-//
-//    // Create a simple layer switcher that toggles layers on
-//    // and off.
-//    var item = document.createElement('li');
-//    var link = document.createElement('a');
-//
-//    link.href = '#';
-//    link.className = 'active';
-//    link.innerHTML = "TEST LAYER";
-//    item.appendChild(link);
-//    ui.appendChild(item);
-
-}
 
 /*
   Handle Sign Up, Login, Logout
@@ -209,7 +174,7 @@ function geoLocationMe(){
                     coordinates: [e.latlng.lng, e.latlng.lat]
                 },
                 "properties": {
-                    "title": "Small kitten",
+                    "title": "Interview",
                     "icon": {
                         "iconUrl": "/static/img/playermarker_" + team.toLowerCase() +".png",
                         "iconSize": [50, 50], // size of the icon
