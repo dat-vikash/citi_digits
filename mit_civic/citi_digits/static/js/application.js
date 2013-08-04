b3c77e2c4da17ae44ff3d4ec2e2c592b92677471
Vikash Dat <vikash.dat@controlgroup.com>
2013-08-04 18:01:21 -0400

    some work on explainations

--- mit_civic/citi_digits/static/js/application.js
+++ mit_civic/citi_digits/static/js/application.js
@@ -204,6 +204,108 @@ function drawNetGainLossGraph(popupId,winnings,spendings, net){
 }
 
 
+function drawPercentIncomeGraphForExplain(medianIncome){
+    console.log("IN HERE");
+    var data = [500,medianIncome];
+    console.log(data);
+
+    //draw top tooltip
+    $("#mapPopupModal #explain-chart #explain-chart-text").append("<b>$" + data[1] + "</b> median household income per day.");
+
+    //draw graph
+     var chart = d3.select("#mapPopupModal #explain-chart").append("svg")
+     .attr("class", "chart")
+     .attr("width", 580)
+     .attr("height", 60);
+
+
+    var x = d3.scale.linear()
+     .domain([0, 500])
+     .range([0, 580]);
+
+    chart.selectAll("rect")
+     .data(data)
+   .enter().append("rect")
+     .attr("width", x)
+        .attr("y",4)
+     .attr("height", 20);
+
+
+    chart.selectAll("line")
+     .data(x.ticks(10))
+   .enter().append("line")
+     .attr("x1", x)
+     .attr("x2", x)
+     .attr("y1", 4)
+     .attr("y2", 24)
+     .style("stroke", "#ffffff");
+
+
+    chart.selectAll(".rule")
+     .data(x.ticks(10))
+   .enter().append("text")
+     .attr("class", "rule")
+     .attr("x", x)
+        .attr("dx",-10)
+     .attr("y", 27)
+     .attr("dy", 7)
+        .attr("text-anchor", "start")
+     .text(function(d,i){ if(i%2==0 && i!=0){return "$"+d;}});
+
+    //determine amount of big braces
+    fullBraceCount = Math.floor(medianIncome/100);
+    smallBraceWidth = (medianIncome/100) % 1;
+    xCoord = 116;
+    yCoord =30;
+    for(var i=0;i<fullBraceCount;i++){
+        //draw full braces
+        console.log("ASdfas");
+        chart.selectAll("path").data({'x1':xCoord - 116,'y1':30,'x2':xCoord,'y2':yCoord}).
+            enter().append("path").attr("class","curlyBrace").attr("d",function (d) {return makeCurlyBrace(d.x1,d.y1,d.x2,d.y2,50,0.6);});
+        xCoord = xCoord + 116;
+    }
+
+
+//    //draw bottom tooltip
+//    $("#map-popup-" + popupId + " #map-popup-graphic #percent_income_graph_text").append("<b>" + data[2].toFixed(1) + "%</b> income spent on lottery (or $" +(data[2].toFixed(1)) +
+//    "out of every $100.");
+
+
+}
+
+
+//returns path string d for <path d="This string">
+		//a curly brace between x1,y1 and x2,y2, w pixels wide
+		//and q factor, .5 is normal, higher q = more expressive bracket
+		function makeCurlyBrace(x1,y1,x2,y2,w,q)
+		{
+			//Calculate unit vector
+			var dx = x1-x2;
+			var dy = y1-y2;
+			var len = Math.sqrt(dx*dx + dy*dy);
+			dx = dx / len;
+			dy = dy / len;
+
+			//Calculate Control Points of path,
+			var qx1 = x1 + q*w*dy;
+			var qy1 = y1 - q*w*dx;
+			var qx2 = (x1 - .25*len*dx) + (1-q)*w*dy;
+			var qy2 = (y1 - .25*len*dy) - (1-q)*w*dx;
+			var tx1 = (x1 -  .5*len*dx) + w*dy;
+			var ty1 = (y1 -  .5*len*dy) - w*dx;
+			var qx3 = x2 + q*w*dy;
+			var qy3 = y2 - q*w*dx;
+			var qx4 = (x1 - .75*len*dx) + (1-q)*w*dy;
+			var qy4 = (y1 - .75*len*dy) - (1-q)*w*dx;
+
+    	return ( "M " +  x1 + " " +  y1 +
+         		" Q " + qx1 + " " + qy1 + " " + qx2 + " " + qy2 +
+          		" T " + tx1 + " " + ty1 +
+          		" M " +  x2 + " " +  y2 +
+          		" Q " + qx3 + " " + qy3 + " " + qx4 + " " + qy4 +
+          		" T " + tx1 + " " + ty1 );
+		}
+
 
 function getPopupUrlFrom(activeLayer,properties){
         name = properties.N_Name.toString().split(' ').join('_');
@@ -763,6 +865,13 @@ $(".map-popup").on("click", "#math_explain", function (ev) {
         }
     }); // display the modal on url load
    });
+
+    $("#mapPopupModal").on("shown",function(){
+        console.log("im shown");
+        drawPercentIncomeGraphForExplain($("#median_income_value").val());
+
+
+    });
     console.log("I AM IN HERE MAN!!");
     return false;
 });
