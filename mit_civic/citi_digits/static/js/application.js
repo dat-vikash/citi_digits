2a00c163e7917c13d6729c65bb933e87431f2d50
Vikash Dat <vikash.dat@controlgroup.com>
2013-08-10 16:33:54 -0400

    clean up of tour preview

--- mit_civic/citi_digits/static/js/application.js
+++ mit_civic/citi_digits/static/js/application.js
@@ -1250,15 +1250,13 @@ $("#addTour").on("click","#save_preview_button",function(ev){
     $("#tourPreview").load(url,function() { // load the url into the modal
             $(this).modal('show').css({
                  width: '100%',
-                 'max-width':'95%',
-                  height:'100%',
-                    'max-height':'90%',
+                 'max-width':'940px',
+                    'max-height':'900px',
                     'top':'5%',
-                    'left':'2%'
-//                  'margin-left': function () {
-//
-//            return (window.width/2)-($(this).width()/2);
-//        }
+//                    'left':'2%',
+                  'margin-left': function () {
+                    return window.pageXOffset+(document.width - (document.width - 940/2))/2;
+                }
     }); // display the modal on url load
 
    });
