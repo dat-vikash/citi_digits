cd3f9c774df6ac095383eb656b8840dfb51e4e1d
Vikash Dat <vikash.dat@controlgroup.com>
2013-08-25 15:11:10 -0400

    fixed issue

--- mit_civic/citi_digits/static/js/CityDigitsMaps.js
+++ mit_civic/citi_digits/static/js/CityDigitsMaps.js
@@ -81,6 +81,11 @@ CityDigitsMap.loadLayerFor = function(layerId){
         if(CURRENT_LAYER == "VIEW_ALL_SCHOOLS" && VIEW_ALL_SCHOOLS_IS_OPEN==true){
             $(".map-ui li.active #map-ui-subnav-content").hide();
             VIEW_ALL_SCHOOLS_IS_OPEN = false;
+            //remove previous layer
+            if(MARKER_LAYER!=null){
+                console.log("clearing layer");
+                MY_MAP.map.removeLayer(MARKER_LAYER);
+            }
         }else{
             CURRENT_LAYER = "VIEW_ALL_SCHOOLS";
             VIEW_ALL_SCHOOLS_IS_OPEN = true;
