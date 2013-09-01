2f01c83a88e1101dd1f8f4153bf04851120b4460
Vikash Dat <vikash.dat@controlgroup.com>
2013-09-01 16:31:14 -0400

    left align city with address fields

--- mit_civic/citi_digits/forms.py
+++ mit_civic/citi_digits/forms.py
@@ -8,13 +8,13 @@ from django.contrib.localflavor.us.us_states import STATE_CHOICES
 
 
 class SignUpForm(forms.Form):
-    firstName = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'First Name', 'class':'sign_up_medium'}))
-    lastName = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Last Name', 'class':'sign_up_medium'}))
+    firstName = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'First Name', 'class':'sign_up_medium','style':'width:150px;'}))
+    lastName = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Last Name', 'class':'sign_up_medium','style':'width:150px;'}))
     password = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Password','class':'sign_up_large'}))
     email = forms.EmailField(widget=forms.TextInput(attrs={'placeholder': 'Email','class':'sign_up_large'}))
     schoolName = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'School Name','class':'sign_up_large'}))
     schoolAddress = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'School Address','class':'sign_up_large'}))
-    schoolCity = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'City','class':'sign_up_medium'}))
+    schoolCity = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'City','class':'sign_up_medium','style':'width:150px; margin-left: -5px;'}))
     schoolState = USStateField(widget=forms.Select(
         choices=STATE_CHOICES,attrs={'placeholder':'State', 'class':'sign_up_medium'}))
     className = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Class Name','class':'sign_up_large'}))
