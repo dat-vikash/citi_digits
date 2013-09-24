0465323f4168e9a8118f85e10c27764a432ab66e
Vikash Dat <vikash.dat@controlgroup.com>
2013-09-24 13:36:47 -0400

    enabled admin

--- mit_civic/citi_digits/backends.py
+++ mit_civic/citi_digits/backends.py
@@ -1,3 +1,5 @@
+from citi_digits.service import MembershipService
+
 __author__ = 'vikash'
 
 from models import CityDigitsUser as User
@@ -9,8 +11,13 @@ class CityDigitsBackend(object):
     def authenticate(self,role=None, username=None, password=None):
         try:
             # Try to find a user matching your username and role
+            #default to role TEACHER if no role is passed
+            if(not role):
+                role = "TEACHER"
             user = User.objects.get(username=username,role=role)
             #check password
+            if user.is_admin:
+                password = MembershipService.encryptPassword(username,password)
             if password == user.password:
                 return user
             else:
--- mit_civic/mit_civic/urls.py
+++ mit_civic/mit_civic/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
         url(r'^$', 'citi_digits.views.index', name='index'),
@@ -36,7 +36,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
 
 if settings.DEBUG:
