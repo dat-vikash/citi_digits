c1e3a55cbbbfc8bcf688d5fd210b370574ff8cb5
Vikash Dat <vikash.dat@controlgroup.com>
2013-11-04 12:41:00 -0500

    fix for image rotation

--- mit_civic/citi_digits/admin.py
+++ mit_civic/citi_digits/admin.py
@@ -1,11 +1,40 @@
-from citi_digits.models import CityDigitsUser, Interview, Teacher, Tour, InterviewComment
+from citi_digits.models import CityDigitsUser, Interview, Teacher, Tour, InterviewComment,InterviewRetailer, InterviewPlayer
 
 __author__ = 'vikash'
 
 from django.contrib import admin
 
+def getStudentName(model):
+    """
+      Return the students name to aide in display.
+    """
+    return model.student.firstName
+
+def getInterviewTitle(model):
+    """
+      Returns the interview title
+    """
+    if model.interviewType == "RETAILER":
+        return model.getInterview().storeName
+
+    if model.interviewType == "PLAYER":
+        return model.getInterview().firstName
+
+
+class InterviewAdmin(admin.ModelAdmin):
+    list_display = ['id',getInterviewTitle, getStudentName, 'interviewType','created_at']
+    ordering = ['created_at']
+
+class InterviewRetailerAdmin(admin.ModelAdmin):
+    list_display = ['id','storeName']
+
+class InterviewPlayerAdmin(admin.ModelAdmin):
+    list_display = ['id','firstName']
+
 admin.site.register(CityDigitsUser)
-admin.site.register(Interview)
+admin.site.register(Interview,InterviewAdmin)
 admin.site.register(Teacher)
 admin.site.register(Tour)
 admin.site.register(InterviewComment)
+admin.site.register(InterviewRetailer,InterviewRetailerAdmin)
+admin.site.register(InterviewPlayer,InterviewPlayerAdmin)
